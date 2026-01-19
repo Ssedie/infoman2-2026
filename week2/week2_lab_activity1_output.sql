1170980b4d2830983eb00eb75b16573cd2099f44
Ssedie <zedric.rulloda@lorma.edu>
2026-01-19 19:05:39 +0700

    week2_lab_activity1

--- week2/week2_lab_activity1_output.sql
+++ week2/week2_lab_activity1_output.sql
@@ -35,3 +35,30 @@ BEGIN
 END;
 
 $$ LANGUAGE plpgsql;
+
+#3
+CREATE OR REPLACE PROCEDURE book_flight(p_passenger_id NUMERIC, p_flight_id NUMERIC, p_seat_number VARCHAR)
+AS $$
+BEGIN
+	INSERT INTO bookings(passenger_id, flight_id, seat_number)
+	VALUES(p_passenger_id, p_flight_id, p_seat_number);
+END;
+
+$$  LANGUAGE plpgsql
+
+#4
+CREATE OR REPLACE PROCEDURE increase_prices_for_airline(p_airline_id NUMERIC, p_percentage_increase NUMERIC)
+AS $$
+DECLARE
+	increased_price RECORD;
+BEGIN
+
+	FOR increased_price IN SELECT flight_id, base_price FROM flights WHERE airline_id = p_airline_id LOOP
+		UPDATE flights
+		SET base_price = base_price * (1 + p_percentage_increase / 100)
+		WHERE flight_id = increased_price.flight_id;
+	END LOOP;
+
+END;
+
+$$ LANGUAGE plpgsql;
